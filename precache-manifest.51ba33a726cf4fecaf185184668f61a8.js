bc370358743e5194ee2462e9d6d2280911be66bb
root <root@gj-isombe.localdomain>
2020-06-03 18:36:26 -0400

    Updates

---
+++ precache-manifest.51ba33a726cf4fecaf185184668f61a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0256fc5d1c53f34f9a2665c4e9e702c",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "2a7a04c6ab9d31da8407",
+    "url": "/smartbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "39ae9a4a0788b51288e2",
+    "url": "/smartbrain/static/css/main.c10f94f3.chunk.css"
+  },
+  {
+    "revision": "2a7a04c6ab9d31da8407",
+    "url": "/smartbrain/static/js/2.d4ddc950.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartbrain/static/js/2.d4ddc950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ae9a4a0788b51288e2",
+    "url": "/smartbrain/static/js/main.57725bf3.chunk.js"
+  },
+  {
+    "revision": "d5106abbe4fdad77a194",
+    "url": "/smartbrain/static/js/runtime-main.07838e49.js"
+  }
+]);
