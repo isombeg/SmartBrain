1ea445896c35715b57a6e070183ce985b1d0be00
root <root@gj-isombe.localdomain>
2020-06-03 18:43:21 -0400

    Updates

---
+++ precache-manifest.bc60ccb74499fa828630a9acaaa8ba65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5643ae6e681835896668ab4a922ffadb",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "2a7a04c6ab9d31da8407",
+    "url": "/SmartBrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "75017783112be95dcfdb",
+    "url": "/SmartBrain/static/css/main.c10f94f3.chunk.css"
+  },
+  {
+    "revision": "2a7a04c6ab9d31da8407",
+    "url": "/SmartBrain/static/js/2.d4ddc950.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/SmartBrain/static/js/2.d4ddc950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75017783112be95dcfdb",
+    "url": "/SmartBrain/static/js/main.1ed859e7.chunk.js"
+  },
+  {
+    "revision": "4e75eeb5fb454515e472",
+    "url": "/SmartBrain/static/js/runtime-main.7c2911ad.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/smartbrain/precache-manifest.51ba33a726cf4fecaf185184668f61a8.js"
+  "/SmartBrain/precache-manifest.bc60ccb74499fa828630a9acaaa8ba65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/smartbrain/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SmartBrain/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
